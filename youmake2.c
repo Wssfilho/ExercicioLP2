dc86d1ebddb8d5f803931843bf506a591ee1c9b6
Wssfilho <wilsonsa004@gmail.com>
2023-04-09 15:33:16 -0300

    mudanca

--- youmake2.c
+++ youmake2.c
@@ -3,18 +3,10 @@
 
 int main(void)
 {
-    int v[5] = {1, 2, 3, 4, 5}, i;
-    int *Ptrv;
-
-    Ptrv = v;
-    printf("%d\n", *Ptrv);
-    Ptrv += 4;
-    printf("%d\n", *Ptrv);
-    for (i = 0; i < 4; i++)
-    {
-        Ptrv--;
-        printf("%d\n", *Ptrv);
-    }
-    system("PAUSE");
+    int x, *p = NULL;
+    p = &x;
+    x = 10;
+    *p = x;
+    printf("%d", *p);
     return 0;
 }
