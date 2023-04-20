ce0d09c0ec704e62d9b0396767a373bf7e9078a3
Wssfilho <wilsonsa004@gmail.com>
2023-04-20 15:55:30 -0300

    add

---
+++ Struct/exercicio11.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int *val;
+    int n;
+    int max;
+    int min;
+    float media;
+} TSeq;
+
+int main()
+{
+    TSeq seq;
+    int i, soma = 0;
+
+    printf("Digite a quantidade de elementos da sequência: ");
+    scanf("%d", &seq.n);
+
+    seq.val = (int *)malloc(seq.n * sizeof(int));
+
+    printf("Digite os valores da sequência: ");
+    for (i = 0; i < seq.n; i++)
+    {
+        scanf("%d", &seq.val[i]);
+        soma += seq.val[i];
+        if (i == 0)
+        {
+            seq.max = seq.val[i];
+            seq.min = seq.val[i];
+        }
+        else
+        {
+            if (seq.val[i] > seq.max)
+                seq.max = seq.val[i];
+            if (seq.val[i] < seq.min)
+                seq.min = seq.val[i];
+        }
+    }
+
+    seq.media = (float)soma / seq.n;
+
+    printf("Máximo: %d\n", seq.max);
+    printf("Mínimo: %d\n", seq.min);
+    printf("Média: %.2f\n", seq.media);
+
+    free(seq.val);
+
+    return 0;
+}
--- Struct/exercicio4.c
+++ Struct/exercicio4.c
@@ -8,34 +8,49 @@ o pior estudante e a média da turma. Considere n um valor informado pelo usuár
 */
 typedef struct
 {
-    int matricula;
     char nome[50];
-    int nota;
+    int matricula;
+    float nota;
 
-} Turma;
+} aluno;
 int main(void)
 {
-    int n, i;
-    Turma *alunos;
-    printf("INSIRA O NUMERO DE ALUNOS");
+    int n, i, melhor = 0, pior = 0, rel = 0;
+    float media = 0.0;
+    aluno *turma;
+    printf("INSIRA O NUMERO DE ALUNOS ");
     scanf("%d", &n);
     fflush(stdin);
-    alunos = malloc(n * sizeof(Turma));
-    if (alunos == NULL)
+    turma = malloc(n * sizeof(aluno));
+    if (turma == NULL)
     {
         printf("MEMORIA NAO ALOCADA\n");
         exit(-1);
     }
     for (i = 0; i < n; i++)
     {
-        printf("Insira o nome do aluno [%d]\n", i);
-        gets(alunos[i].nome);
-        
-    }
-    for (i = 0; i < n; i++, alunos++)
-    {
-        printf("Insira o nome do aluno [%s]\n", alunos->nome);
-    }
 
-    free(alunos);
+        system("clear || cls");
+        fflush(stdin);
+        printf("Insira o nome do aluno [%d]: ", i + 1);
+        gets(turma[i].nome);
+        printf("Insira a matricula de %s\n", turma[i].nome);
+        scanf("%d", &turma[i].matricula);
+        printf("Insira a nota\n");
+        scanf("%f", &turma[i].nota);
+        media += turma[i].nota;
+        if (turma[i].nota > turma[melhor].nota)
+        {
+            melhor = i;
+        }
+        if (turma[i].nota < turma[pior].nota)
+        {
+            pior = i;
+        }
+        }
+    media /= n;
+    printf("\nMelhor aluno: %s (Matricula: %d, Nota: %.2f)\n", turma[melhor].nome, turma[melhor].matricula, turma[melhor].nota);
+    printf("Pior aluno: %s (Matricula: %d, Nota: %.2f)\n", turma[pior].nome, turma[pior].matricula, turma[pior].nota);
+    printf("Media da turma: %.2f\n", media);
+    free(turma);
 }
