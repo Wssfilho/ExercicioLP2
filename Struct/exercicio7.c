719a5d9d5b1b32ee55cc2f706076c495f0be148c
Wssfilho <wilsonsa004@gmail.com>
2023-04-27 10:01:16 -0300

    add

--- Struct/exercicio7.c
+++ Struct/exercicio7.c
@@ -4,27 +4,55 @@
 
 typedef struct
 {
-    float x;
-    float y;
-} Tponto;
+    float x, y;
+} TPonto;
 
 int main(void)
 {
-    Tponto p1, p2, p3;
-    float d12, d23, d31;
-    printf("Innsira os valores ");
-    scanf("%f %f", &p1.x, &p1.y);
-    printf("Innsira os valores ");
-    scanf("%f %f", &p2.x, &p2.y);
-    printf("Innsira os valores ");
-    scanf("%f %f", &p3.x, &p3.y);
-    d12 = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.x - p2.y) * (p1.x - p2.y));
-    d23 = sqrt((p2.x - p3.x) * (p2.x - p3.x) + (p2.x - p3.y) * (p2.x - p3.y));
-    d31 = sqrt((p3.x - p1.x) * (p3.x - p1.x) + (p3.x - p1.y) * (p3.x - p1.y));
-    if (d12 > d23 && d12 > d31)
-    {
-        
-    }
+    TPonto P1, P2, P3;
+    float A, B, C;
 
+    printf("Digite a coordenada x do ponto 1: ");
+    scanf("%f", &P1.x);
+    printf("Digite a coordenada y do ponto 1: ");
+    scanf("%f", &P1.y);
+
+    printf("Digite a coordenada x do ponto 2: ");
+    scanf("%f", &P2.x);
+    printf("Digite a coordenada y do ponto 2: ");
+    scanf("%f", &P2.y);
+
+    printf("Digite a coordenada x do ponto 3: ");
+    scanf("%f", &P3.x);
+    printf("Digite a coordenada y do ponto 3: ");
+    scanf("%f", &P3.y);
+
+    A = sqrt((P2.x - P1.x) * (P2.x - P1.x) + (P2.y - P1.y) * (P2.y - P1.y));
+    B = sqrt((P3.x - P2.x) * (P3.x - P2.x) + (P3.y - P2.y) * (P3.y - P2.y));
+    C = sqrt((P1.x - P3.x) * (P1.x - P3.x) + (P1.y - P3.y) * (P1.y - P3.y));
+
+    printf("\nA = %.2f, B = %.2f, C = %.2f\n", A, B, C);
+
+    if (A > B && A > C)
+    {
+        if (A * A == (B * B) + (C * C))
+            printf("É um triângulo retângulo!");
+        else
+            printf("Não é um triângulo retângulo!!");
+    }
+    else if (B > A && B > C)
+    {
+        if (B * B == (A * A) + (C * C))
+            printf("É um triângulo retângulo!");
+        else
+            printf("Não é um triângulo retângulo!!");
+    }
+    else if (C > B && C > A)
+    {
+        if (C * C == (B * B) + (A * A))
+            printf("É um triângulo retângulo!");
+        else
+            printf("Não é um triângulo retângulo!!");
+    }
     return 0;
 }
