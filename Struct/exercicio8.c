6e0340eae29242f1699cd4bc03b4c5d8d4e65da5
Wssfilho <wilsonsa004@gmail.com>
2023-05-01 17:37:48 -0300

    comments

--- Struct/exercicio8.c
+++ Struct/exercicio8.c
@@ -5,19 +5,22 @@
 typedef struct
 {
     float x, y, z;
-} Tponto;
+    //criacao da estrutura com os pontos
+} Tponto; 
 
 int main(void)
 {
-    Tponto p;
-    int A, B, C, D;
-    printf("INSIRA AS COORDENADAS: ");
-    scanf("%d %d %d %d", &A, &B, &C, &D);
+    Tponto p; //declarando variavel do tipo estrutura
+    int A, B, C, D; //declarando variaveis
+    printf("INSIRA AS COORDENADAS: "); //entrada de dados
+    scanf("%d %d %d %d", &A, &B, &C, &D); //entrada de dados
+
     printf("INSIRA OS PONTOS X, Y, Z");
     scanf("%f %f %f", &p.x, &p.y, &p.z);
+
     if (A * p.x + B * p.y + C * p.z == D)
     {
-        printf("PERTENCE AO PLANO A B C");
+        printf("PERTENCE AO PLANO A B C"); //if e else dos calculos que a questao pede e ja da a equacao
     }
     else
     {
