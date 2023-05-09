fce253ee0d5f7bc0fae5ecf199291bed18746189
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-09 10:22:37 -0300

    add

--- funcao/licao.c
+++ funcao/licao.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
+#include <math.h>
 int multiplicacao(int n1, int n2);
+int raiz(int x)
+{
+    int retorno_raiz;
+    retorno_raiz = sqrt(x);
+    return(retorno_raiz);
+}
 int main(void)
 {
-    int v1, v2, resultado;
+    int v1, v2, resultado, result_raiz;
     printf("Insirao primeiro valor: ");
     scanf("%d", &v1);
     printf("Insirao o segundo valor: ");
     scanf("%d", &v2);
-
+    result_raiz = raiz(v1);
     // chamar a funcao e recebe o retorno
     resultado = multiplicacao(v1, v2);
     printf("Resultado: %d\n", resultado);
+    printf("Resultado da raiz: %d\n", result_raiz);
     return 0;
 }
 int multiplicacao(int n1, int n2)
