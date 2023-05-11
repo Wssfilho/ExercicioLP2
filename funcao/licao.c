ec736394478808c373a7fcc44dd097688a0a6bf3
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-11 09:35:58 -0300

    add discrypt

--- funcao/licao.c
+++ funcao/licao.c
@@ -1,30 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-int multiplicacao(int n1, int n2);
-int raiz(int x)
-{
-    int retorno_raiz;
-    retorno_raiz = sqrt(x);
-    return(retorno_raiz);
-}
+
+float **aloca_matriz(const int, const int);
+void prn_oper(float **, float **, float **, const int, const int, const char);
+void liberar_matriz(float **, const int);
 int main(void)
 {
-    int v1, v2, resultado, result_raiz;
-    printf("Insirao primeiro valor: ");
-    scanf("%d", &v1);
-    printf("Insirao o segundo valor: ");
-    scanf("%d", &v2);
-    result_raiz = raiz(v1);
-    // chamar a funcao e recebe o retorno
-    resultado = multiplicacao(v1, v2);
-    printf("Resultado: %d\n", resultado);
-    printf("Resultado da raiz: %d\n", result_raiz);
+    float **A, **B, **som, **sub;
+    int i, j, m, n;
+
+    // leitura do numero de linhas e colunas
+    printf("Digite o numero de linhas da matriz: ");
+    scanf("%d", &m);
+    printf("Digite o numero de colunas da matriz: ");
+    scanf("%d", &n);
+
+    // alocacao dinamica de matrizes;
+    A = aloca_matriz(m, n);
+    B = aloca_matriz(m, n);
+    som = aloca_matriz(m, n);
+    sub = aloca_matriz(m, n);
+
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("Digite o elemento [%d][%d] de A: ", i + 1, j + 1);
+            scanf("%f", &A[i][j]);
+            printf("Digite o elemento [%d][%d] de B: ", i + 1, j + 1);
+            scanf("%f", &B[i][j]);
+            som[i][j] = A[i][j] + B[i][j];
+            sub[i][j] = A[i][j] - B[i][j];
+        }
+    }
+    // resultados
+    printf("\nSOMA\n");
+    prn_oper(A, B, som, m, n, '+');
+    printf("\nSubtracao\n");
+    prn_oper(A, B, som, m, n, '-');
+    // limpando memoria
+    liberar_matriz(A, m);
+    liberar_matriz(B, m);
+    liberar_matriz(som, m);
+    liberar_matriz(sub, m);
+
     return 0;
 }
-int multiplicacao(int n1, int n2)
+float **aloca_matriz(const int l, const int c)
 {
-    int result;
-    result = n1 * n2;
-    return (result);
-} // multiplica recebe n1 e n2 e retorna um inteiro
+    float **m;
+    int i;
+
+    m = (float **)malloc(l * sizeof(float *));
+    if (m == NULL)
+    {
+        printf("ERRO, estouro de memoria");
+        exit(-1);
+    }
+    for (i = 0; i < l; i++)
+    {
+        m[i] = (float *)malloc(c * sizeof(float));
+        if (m[i] == NULL)
+        {
+            printf("Error, estouro de momoria!\n");
+            exit(-1);
+        }
+    }
+    return m;
+}
+void liberar_matriz(float **m, const int l)
+{
+    int i;
+    for (i = 0; i < l; i++)
+        free(m[i]);
+    free(m);
+}
+void prn_oper(float **m1, float **m2, float **m3,
+              const int l, const int c, const char oper)
+{
+    int i, j;
+
+    for (i = 0; i < l; i++)
+    {
+        for (j = 0; j < c; j++)
+            printf("%4.2f ", m1[i][j]);
+        if (i == (l / 2))
+            printf(" %c ", oper);
+        else
+            printf(" ");
+        for (j = 0; j < c; j++)
+            printf("%4.2f ", m2[i][j]);
+        if (i == (l / 2))
+            printf(" = ");
+        else
+            printf(" ");
+        for (j = 0; j < c; j++)
+            printf("%4.2f ", m3[i][j]);
+        printf("\n");
+    }
+}
