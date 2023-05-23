b65b9daaf1d77c7efed5b9eff2a6f0196b78c3dc
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-23 13:25:05 -0300

    add

--- Recursão/aprendizagem.c
+++ Recursão/aprendizagem.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(void);
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+int main() {
+    int n;
+    printf("Digite o número de termos da sequência de Fibonacci: ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+    return 0;
+}
