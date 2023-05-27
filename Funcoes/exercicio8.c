5ed29ea146be1f1e2a879ea459eb47af869bd6f9
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-27 19:18:24 -0300

    add

--- Funcoes/exercicio8.c
+++ Funcoes/exercicio8.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
-int eh_primo(int n)
+#include <stdlib.h>
+int eh_primo(int n) //funcao para determinar se um numero e primo
 {
     if (n <= 1)
         return 0;
-    for (int i = 2; i * i <= n; i++)
+    for (int i = 2; i * i <= n; i++) //condicao para achar o numero primo
     {
         if (n % i == 0)
             return 0;
@@ -12,11 +12,11 @@ int eh_primo(int n)
     return 1;
 }
 
-int main()
+int main(void) //comeco da funcao principal
 {
     for (int i = 1; i <= 1000; i++)
     {
-        if (eh_primo(i))
+        if (eh_primo(i)) //chamada da funcao em que vai verificar todos os numeros entre 1 e 1000, evia parametro em i pra funcao e retorna sim ou nao, sim sim, imprime, se nao, nao imprime;
             printf("%d\n", i);
     }
     return 0;
--- Funcoes/exercicio9.c
+++ Funcoes/exercicio9.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+// Função para verificar se um número é primo
 int eh_primo(int n)
 {
+    // Números menores ou iguais a 1 não são primos
     if (n <= 1)
         return 0;
+    // Verifica se n é divisível por qualquer número menor que sua raiz quadrada
     for (int i = 2; i * i <= n; i++)
     {
         if (n % i == 0)
             return 0;
     }
+    // Se nenhum divisor foi encontrado, n é primo
     return 1;
 }
 
 int main(void)
 {
     int n;
+    // Solicita ao usuário que insira o limite
     printf("Insira o tamanho da impressao de numeros primos: ");
     scanf("%d", &n);
+    // Verifica cada número de 1 a n
     for (int i = 1; i <= n; i++)
     {
+        // Se eh_primo retornar verdadeiro, imprime o número
         if (eh_primo(i))
             printf("%d\n", i);
     }
