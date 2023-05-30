5f16ec0f2340c39524d9d4f24d33a123aa23bcfc
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-30 19:53:07 -0300

    modified:   Funcoes/Aprendizagem.c modified:   Funcoes/output/Aprendizagem.exe

--- Funcoes/Aprendizagem.c
+++ Funcoes/Aprendizagem.c
@@ -1,117 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-// Define a estrutura Taluno com os membros nome, idade e matricula
+#include <math.h>
 typedef struct
 {
-    char nome[50];
-    int idade;
-    int matricula;
-} Taluno;
-//funcoes utilizadas
-void numeroaluno(int *n);
-void opcoes(void);
-Taluno *alocar(const int n);
-void mostrar(Taluno *alunos, const int n);
-void preencher(Taluno *alunos, const int n);
-void comp(Taluno *alunos, const int n);
+    float x, y;
+} TpontoPlano;
+
+float calculopontos(TpontoPlano *estrutura);
+
 int main(void)
 {
-    int opcao = 0;
-    int n;
-    Taluno *alunos;
-    numeroaluno(&n);
-    // Pede ao usuário o número de alunos
-    alunos = alocar(n);
-    system("clear || cls");
-    do
-    {
-        opcoes();
-        scanf("%d", &opcao);
-        switch (opcao)
-        {
-        case 1:
-            // Preenche os dados dos alunos
-            preencher(alunos, n);
-            break;
-        case 2:
-            // Mostra os dados dos alunos
-            mostrar(alunos, n);
-            break;
-        case 3:
-            // mostar o aluno mais novo
-            comp(alunos, n);
-            break;
-        }
-
-    } while (opcao);
-    // Libera a memória alocada
-    free(alunos);
-
+    TpontoPlano a;
+    float ponto;
+    ponto = calculopontos(&a);
+    printf("Os pontos que o jogador recebe e: %.1f", ponto);
     return 0;
 }
-
-//
-
-void numeroaluno(int *n)
+float calculopontos(TpontoPlano *estrutura)
 {
-    opcoes();
-    printf("Digite o número de alunos: ");
-    scanf("%d", n);
-}
-// funcao menu
-void opcoes(void)
-{
-    printf("------ ESCOLHA UMA DAS OPCOES------\n\n");
-    printf("0. *Sair do sistema* \n");
-    printf("1. Inserir aluno: \n");
-    printf("2. Mostrar aluno: \n");
-    printf("3. Comparar alunos e imprimir o mais novo: \n");
-}
-// Função para alocar dinamicamente um array de Taluno de tamanho n
-Taluno *alocar(const int n)
-{
-    Taluno *alunos = malloc(n * sizeof(Taluno));
-    if (alunos == NULL)
-    {
-        printf("Erro de memoria: ");
-        exit(-1);
-    }
-    return alunos;
-}
-// Função para preencher os dados dos alunos
-void preencher(Taluno *alunos, const int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        fflush(stdin);
-        printf("Digite o nome do aluno %d: ", i + 1);
-        scanf("%[^\n]s%*c", alunos[i].nome);
-        printf("Digite a idade do aluno %d: ", i + 1);
-        scanf("%d", &alunos[i].idade);
-        printf("Digite a matricula do aluno %d: ", i + 1);
-        scanf("%d", &alunos[i].matricula);
-    }
-}
-// Função para mostrar os dados dos alunos
-void mostrar(Taluno *alunos, const int n)
-{
-    for (int i = 0; i < n; i++)
-    {
-        printf("Aluno %d\n", i + 1);
-        printf("Nome: %s\n", alunos[i].nome);
-        printf("Idade: %d\n", alunos[i].idade);
-        printf("Matricula: %d\n", alunos[i].matricula);
-    }
-}
-void comp(Taluno *alunos, const int n)
-{
-    int aluno_novo = 0;
-    for (int i = 1; i < n; i++)
-    {
-        if (alunos[i].idade < alunos[aluno_novo].idade)
-        {
-            aluno_novo++;
-        }
-    }
-    printf("O aluno mais novo e: %s", alunos[aluno_novo].nome);
+    float pontos = 0;
+    printf("Insira os pontos x e y respectivamente: ");
+    scanf("%f %f", &estrutura->x, &estrutura->y);
+    float d = sqrt((estrutura->x * estrutura->x) + (estrutura->y * estrutura->y));
+    if (d == 0)
+        pontos = 20;
+    else if (d < 10)
+        pontos = 10;
+    else if (d > 10 && d <= 25)
+        pontos = 5;
+    else if (d > 25 && d <= 50)
+        pontos = 2;
+    else
+        pontos -= 1;
+    return pontos;
 }
