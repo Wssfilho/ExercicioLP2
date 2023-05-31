23e031494927e6d15238c9843a03cda60ef231bc
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-05-31 14:59:08 -0300

    modified:   "Recurs\303\243o/exercicio8.1.c"

--- Recursão/exercicio8.1.c
+++ Recursão/exercicio8.1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-void busca(int val); // declaracao da funcao que recebe um inteiro e nao retorna nada
+void busca(int n); // declaracao da funcao que recebe um inteiro e nao retorna nada
 int main(void)
 {
     int val;
@@ -11,12 +11,12 @@ int main(void)
 }
 void busca(int n)
 {
-    if (n >= 2)       // como eu falei em sala de aula, esse algoritimo ele suprime o zero que aparece primeiro
-        busca(n / 2); // no algoritimo abaixo o returno seria 3 = 011, nesse sai 3 = 11;
-    printf("%d", n % 2);
+    //if (n >= 2)       // como eu falei em sala de aula, esse algoritimo ele suprime o zero que aparece primeiro
+    //    busca(n / 2); // no algoritimo abaixo o returno seria 3 = 011, nesse sai 3 = 11;
+    //printf("%d", n % 2);
 
-    /*
-    if (n == 0)
+    
+    if (n > 0)
        busca(n / 2);
-    printf("%d", n % 2); */
+    printf("%d", n % 2); 
 }
