2c33b72bc75dcdf8c8688ca1374efa08ba5bcef8
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-06-07 16:37:48 -0300

    modified:   arquivos/aprenizagem.c modified:   arquivos/output/aprenizagem.exe new file:   arquivos/output/bd.txt deleted:    arquivos/output/pessoas.txt

--- arquivos/aprenizagem.c
+++ arquivos/aprenizagem.c
@@ -1,56 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
-typedef struct {
-    char nome[50];
+typedef struct
+{
+    char nome[100];
     int idade;
-} Pessoa;
+} Tpessoa;
+Tpessoa *alocacao(void);
+void inserir(Tpessoa *);
+void menu(int *opcao);
+void mostrar(Tpessoa *vector);
 
-void inserir(FILE *arquivo) {
-    Pessoa *p = (Pessoa*) malloc(sizeof(Pessoa));
-    printf("Digite o nome: ");
-    scanf("%s", p->nome);
-    printf("Digite a idade: ");
-    scanf("%d", &p->idade);
-    fprintf(arquivo, "%s %d\n", p->nome, p->idade);
-    free(p);
-}
-
-void mostrar(FILE *arquivo) {
-    system("clear || cls");
-    Pessoa p;
-    rewind(arquivo);
-    while(fscanf(arquivo, "%s %d\n", p.nome, &p.idade) != EOF) {
-        printf("Nome: %s\n", p.nome);
-        printf("Idade: %d\n", p.idade);
-    }
-}
-
-int main() {
-    FILE *arquivo = fopen("pessoas.txt", "a+");
-    int opcao;
-    do {
-        printf("1 - Inserir\n");
-        printf("2 - Mostrar\n");
-        printf("3 - Sair\n");
-        printf("Opcao: ");
-        scanf("%d", &opcao);
-        switch(opcao) {
-            case 1:
-                inserir(arquivo);
-                break;
-            case 2:
-                mostrar(arquivo);
-                sleep(5);
-                system("clear || cls");
-                break;
-            case 3:
-                break;
-            default:
-                printf("Opcao invalida!\n");
+int main(void)
+{
+    Tpessoa *dados;
+    int opcao = 0;
+    do
+    {
+        menu(&opcao);
+        switch (opcao)
+        {
+        case 1:
+            system("clear || cls");
+            dados = alocacao();
+            inserir(dados);
+            printf("0 pra sair e 1 pra cadastrar novamente");
+            scanf("%d", &opcao);
+            while (opcao != 0 && opcao != 1)
+            {
+                printf("Digite 0 ou 1");
+                scanf("%d", &opcao);
+            }
+            break;
+        case 2:
+            mostrar(dados);
+            printf("1. Voltar ao menu? 0. Sair");
+            scanf("%d", &opcao);
+            while (opcao != 0 || opcao != 1)
+            {
+                printf("Digite 0 ou 1");
+                scanf("%d", &opcao);
+            }
+            break;
+        default:
+            printf("Esolha uma das opcoes: ");
+            scanf("%d", &opcao);
+            break;
         }
-    } while(opcao != 3);
+    } while (opcao != 0);
+}
+void mostrar(Tpessoa *vector)
+{
+    system("clear || cls");
+    FILE *arquivo;
+    if ((arquivo = fopen("bd.txt", "r")) == NULL)
+        exit(-1);
+    rewind(arquivo);
+    while (fscanf(arquivo, "%s %d\n", vector->nome, &vector->idade) != EOF)
+    {
+        printf("Nome: %s\n", vector->nome);
+        printf("Idade: %d\n", vector->idade);
+    }
     fclose(arquivo);
-    return 0;
+}
+void inserir(Tpessoa *dados)
+{
+    FILE *arquivo;
+    if ((arquivo = fopen("bd.txt", "a+")) == NULL)
+        exit(-1);
+    fflush(stdin);
+    printf("Insira seu nome: ");
+    scanf("%[^\n]s", dados->nome);
+    scanf("%*c");
+    printf("Insira sua idade: ");
+    scanf("%d", &dados->idade);
+    fprintf(arquivo, "%s\n %d\n", dados->nome, dados->idade);
+    fclose(arquivo);
+}
+Tpessoa *alocacao(void)
+{
+    Tpessoa *temp;
+    temp = (Tpessoa *)malloc(sizeof(Tpessoa));
+    if (temp == NULL)
+    {
+        printf("Memoria nao alocada, procure o desenvolvedor: ");
+        exit(-1);
+    }
+    return temp;
+}
+void menu(int *opcao)
+{
+    printf("BEM VINDO\n");
+    printf("0. SAIR\n");
+    printf("1. INSERIR\n");
+    printf("2. MOSTAR\n");
+    scanf("%d", opcao);
 }
