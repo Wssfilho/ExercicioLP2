598bf281e9db94c2307da7afe0e43fde89975f3d
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-06-08 11:07:21 -0300

    .

--- Projeto/Meu primeiro site/inde.c
+++ Projeto/Meu primeiro site/inde.c
@@ -3,21 +3,36 @@
 #include <time.h>
 int *vectoralloc(const int n);
 void values(int *, const int);
-int inverter(int *, const int);
+void inverter(int *, int *, const int);
+void arq(const int *const, const int);
 int main(void)
 {
     srand(time(NULL));
-    int *vector, n;
-    //int* pointer;
+    int *vector, *newarray, n;
     printf("Insira o tamanho do vetor: ");
     scanf("%d", &n);
     vector = vectoralloc(n);
+    newarray = vectoralloc(n);
     values(vector, n);
-    inverter(vector, n);
-    //n; remove the function above and place here;
-    //pointer = &n;
+    inverter(vector, newarray, n);
+    arq(vector, n);
+    free(newarray);
     free(vector);
     return 0;
+}
+void arq(const int *const vector, const int n)
+{
+    FILE *vectorarq;
+    if ((vectorarq = fopen("vectorbd.txt", "w")) == NULL)
+    {
+        printf("WARNING: O arquivo nao foi aberto ou criado!");
+        exit(-1);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(vectorarq, "%d", vector[i]);
+    }
+    fclose(vectorarq);
 }
 int *vectoralloc(const int n)
 {
@@ -34,21 +49,20 @@ void values(int *vector, int n)
         scanf("%d", &vector[i]);
     }
 }
-int inverter(int *array, const int n)
+void inverter(int *array, int *swap, const int n)
 {
+    for (int num = 0; num < n; num++)
+        swap[num] = array[num];
+    
     int i, k, aux;
     printf("\n");
     for (i = 0; i < n; i++)
     {
         k = rand() % n;
-        aux = array[i];
-        array[i] = array[k];
-        array[k] = aux;
+        aux = swap[i];
+        swap[i] = swap[k];
+        swap[k] = aux;
     }
     for (i = 0; i < n; i++)
-        printf("[%d] O valor e: %d\n", i + 1, array[i]);
-    //int *pointer;
-    //pointer = array;
-    //return *pointer;
-    return 0;
+        printf("[%d] O valor e: %d\n", i + 1, swap[i]);
 }
