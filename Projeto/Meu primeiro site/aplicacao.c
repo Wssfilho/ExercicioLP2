1e60e547458cf7dfc48755e8d2b5e462a0d7773e
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-07-04 18:16:00 -0300

    modified:   Projeto/Meu primeiro site/aplicacao.c 	new file:   Projeto/Meu primeiro site/output/aplicacao.exe 	new file:   Projeto/Meu primeiro site/output/dados.txt

--- Projeto/Meu primeiro site/aplicacao.c
+++ Projeto/Meu primeiro site/aplicacao.c
@@ -6,24 +6,86 @@ typedef struct
     char nome[40];
     int idade;
 } Tpessoa;
-Tpessoa *allocar();
+Tpessoa *allocar(void);
 void menu(int *);
+void inserir(Tpessoa *);
+void gravar(Tpessoa *);
+void mostrar(Tpessoa *);
 int main(void)
 {
     int opcao;
-    Tpessoa *dado;
-    dado = allocar();
-    switch(opcao)
+    Tpessoa *dado = allocar();
+    do
     {
-        
-    }
+        menu(&opcao);
+        switch (opcao)
+        {
+        case 1:
+            inserir(dado);
+            gravar(dado);
+            break;
+        case 2:
+            mostrar(dado);
+            break;
+        default:
+            break;
+        }
+    } while (opcao != 0);
+    free(dado);
+    return 0;
 }
-Tpessoa *allocar()
+void mostrar(Tpessoa *dado)
+{
+    FILE *arq;
+    if((arq = fopen("dados.txt", "r")) == NULL)
+    {
+        printf("O arquivo nao foi aberto");
+        exit(-1);
+    }
+    while (fscanf(arq, "%s %d", dado->nome, &dado->idade) != EOF)
+    {
+        printf("Nome: %s\n", dado->nome);
+        printf("Idade: %d\n\n", dado->idade);
+    }
+    
+}
+void menu(int *opcao)
+{
+    printf("0. Sair\n");
+    printf("1. Inserir\n");
+    printf("2. Mostrar\n");
+    printf("Escolha uma das opcoes acima: ");
+    scanf("%d", opcao);
+}
+Tpessoa *allocar(void)
 {
     Tpessoa *temp;
     temp = malloc(sizeof(Tpessoa));
     if (temp == NULL)
+    {
         printf("Nao foi alocado");
         exit(-1);
+    }
     return temp;
+}
+void inserir(Tpessoa *dados)
+{
+    fflush(stdin);
+    printf("Insira seu nome: ");
+    scanf("%[^\n]s", dados->nome);
+    fflush(stdin);
+    printf("Insira sua idade: ");
+    scanf("%d", &dados->idade);
+}
+void gravar(Tpessoa *dado)
+{
+    FILE *arq;
+    if ((arq = fopen("dados.txt", "a+")) == NULL)
+    {
+        system("clear || cls");
+        printf("Nao foi aberto o arquivo! Saindo");
+        exit(-1);
+    }
+    fprintf(arq, "%s %d\n", dado->nome, dado->idade);
+    fclose(arq);
 }
