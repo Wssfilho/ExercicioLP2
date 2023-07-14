c15c056715b3b84e163eace519d854888bc14114
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-07-14 12:00:07 -0300

    Changes to be committed: 	new file:   Projeto/Meu aplicativo de cadastro v1.0/.vscode/c_cpp_properties.json 	new file:   Projeto/Meu aplicativo de cadastro v1.0/.vscode/launch.json 	new file:   Projeto/Meu aplicativo de cadastro v1.0/.vscode/settings.json 	new file:   Projeto/Meu aplicativo de cadastro v1.0/app.c 	new file:   Projeto/Meu aplicativo de cadastro v1.0/output/app.exe

---
+++ Projeto/Meu aplicativo de cadastro v1.0/app.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char nome[30];
+    int idade;
+    float nota;
+
+} Tdado;
+void menu(void);
+void choose(int *);
+Tdado *allc(void);
+void inserir(Tdado *);
+
+//******MAIN********
+int main(void)
+{
+    menu();
+    return 0;
+}
+//******MAIN********
+void inserir(Tdado *dado)
+{
+    scanf("%*c");
+    printf("Insira o nome:");
+    scanf("%[^\n]s", dado->nome);
+    scanf("%*c");
+    printf("Insira sua idade: ");
+    scanf("%d", &dado->idade);
+    printf("Insira sua nota: ");
+    scanf("%f", &dado->nota);
+}
+void choose(int *opcao)
+{
+    Tdado *dado;
+    dado = allc();
+    do
+    {
+        switch (*opcao)
+        {
+        case 1:
+            inserir(dado);
+            break;
+        default:
+            break;
+        }
+        menu();
+    } while (opcao != 0);
+    
+    free(dado);
+}
+Tdado *allc(void)
+{
+    Tdado *temp;
+    temp = (Tdado *) malloc(sizeof(Tdado));
+    if(temp == NULL)
+    {
+        printf("A alocacao foi mal sucedida");
+        exit(-1);
+    }
+    return temp;
+}
+void menu(void)
+{
+    int opcao;
+    printf("----Sistema de cadastro 1.0----\n\n");
+    printf("1. Inserir\n");
+    printf("2. Mostrar\n");
+    // printf()
+    // printf()
+    // printf()
+    // printf()
+    printf("Escolha uma das opcoes: ");
+    scanf("%d", &opcao);
+    while(opcao > 5 || opcao < 0)
+    {
+        printf("Escolha a opcao correta: ");
+        scanf("%d", &opcao);
+    }
+    choose(&opcao);
+    
+}
