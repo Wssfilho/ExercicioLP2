ea1506bd5b0029bbcaee03ee3e1ba20b897ee542
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-10-16 13:29:03 -0300

    Changes to be committed: 	modified:   trabalho lpIII/index.html 	modified:   trabalho lpIII/index.js

--- trabalho lpIII/index.js
+++ trabalho lpIII/index.js
@@ -1,33 +1,35 @@
-
-class Conta {
-
-    constructor(numero, saldo = 0) {
-        this.numero = numero;
-        this.saldo = saldo;
+// Definição da classe
+class Pessoa {
+    // Construtor
+    constructor(nome, idade) {
+      this.nome = nome;
+      this.idade = idade;
     }
-
-    depositar(valor) {
-        this.saldo += valor;
+  
+    // Método para imprimir os dados da pessoa
+    imprimirDados() {
+      const dados = `Nome: ${this.nome}, idade: ${this.idade}`;
+      document.querySelector("p").textContent = dados;
     }
-
-    sacar(valor) {
-        if (valor <= this.saldo) {
-            this.saldo -= valor;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    consultarSaldo() {
-        return this.saldo;
-    }
-
-    imprimir() {
-        const dados = `Nome: ${this.numero}, idade: ${this.saldo}`;
-        document.querySelector("p").textContent = dados;
-    }
-}
-
-var minhaConta = new Conta(12345, 1000);
-document.querySelector("button").addEventListener("click", pessoa.imprimirDados);
+  }
+  
+  // Criação de uma instância da classe
+  const pessoa = new Pessoa();
+  
+  // Adicionando um evento de submit ao formulário
+  document.querySelector("form").addEventListener("submit", function(e) {
+    // Cancelando o envio do formulário
+    e.preventDefault();
+  
+    // Obtendo os dados do formulário
+    const nome = document.querySelector("input[name='nome']").value;
+    const idade = document.querySelector("input[name='idade']").value;
+  
+    // Colocando os dados no objeto
+    pessoa.nome = nome;
+    pessoa.idade = idade;
+  
+    // Imprimindo os dados da pessoa
+    pessoa.imprimirDados();
+  });
+  
